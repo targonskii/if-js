e894c77185e84fcfb038cf3e978e5380ba462a9f
targonskii <polarpando@gmail.com>
2021-07-15 20:42:30 +0300

    Changed class, shortened the code

--- lesson-11/main.js
+++ lesson-11/main.js
@@ -5,7 +5,7 @@ const roomsInput = document.querySelector('.header__input_rooms');
 const peopleFilter = document.querySelector('.header__people_filter');
 
 const focusPeople = () => {
-  peopleFilter.classList.toggle('toggle_filter');
+  peopleFilter.classList.toggle('visible_filter');
 };
 peopleInput.addEventListener('click', focusPeople);
 
@@ -15,8 +15,8 @@ const childrenFilter = document.querySelector('.header__people_age');
 
 const showChildren = (e) => {
   childrenFilter.style.display = 'block';
-
-  if (e.target.parentElement.querySelector('input').value >= 1 && e.target.parentElement.querySelector('input').value <= 9) {
+  const targetInput = e.target.parentElement.querySelector('input');
+  if (targetInput.value >= 1 && targetInput.value <= 9) {
     const select = document.querySelector('select').cloneNode(true);
     document.querySelector('select').after(select);
   }
@@ -39,84 +39,87 @@ const minusRoom = document.querySelector('.header__people_rooms button.minus');
 const plusRoom = document.querySelector('.header__people_rooms button.plus');
 
 const clickAdults = (e) => {
+  const targetAdultsInput = e.target.parentElement.querySelector('.header__people_adults input');
   if (e.target.classList.contains('plus')) {
-    ++e.target.parentElement.querySelector('.header__people_adults input').value;
+    ++targetAdultsInput.value;
   } else if (e.target.classList.contains('minus')) {
-    --e.target.parentElement.querySelector('.header__people_adults input').value;
+    --targetAdultsInput.value;
   }
-  if (e.target.parentElement.querySelector('.header__people_adults input').value <= 0) {
-    e.target.parentElement.querySelector('.header__people_adults input').value = 0;
+  if (targetAdultsInput.value <= 0) {
+    targetAdultsInput.value = 0;
     minus.style.border = '1px solid #CECECE';
     minus.style.color = '#CECECE';
   } else {
     minus.style.border = '1px solid #3077c6';
     minus.style.color = '#3077c6';
   }
-  if (e.target.parentElement.querySelector('.header__people_adults input').value >= 30) {
-    e.target.parentElement.querySelector('.header__people_adults input').value = 30;
+  if (targetAdultsInput.value >= 30) {
+    targetAdultsInput.value = 30;
     plus.style.border = '1px solid #CECECE';
     plus.style.color = '#CECECE';
   } else {
     plus.style.border = '1px solid #3077c6';
     plus.style.color = '#3077c6';
   }
-  adultsInput.setAttribute('placeholder', `${e.target.parentElement.querySelector(('.header__people_adults input')).value}` + ' Adults     — ');
+  adultsInput.setAttribute('placeholder', `${targetAdultsInput.value}` + ' Adults     — ');
 };
 
 plus.addEventListener('click', clickAdults);
 minus.addEventListener('click', clickAdults);
 
 const clickChildBtn = (e) => {
+  const targetChildrenInput = e.target.parentElement.querySelector('.header__people_children input');
   if (e.target.classList.contains('plus')) {
-    ++e.target.parentElement.querySelector('.header__people_children input').value;
+    ++targetChildrenInput.value;
   } else if (e.target.classList.contains('minus')) {
-    --e.target.parentElement.querySelector('.header__people_children input').value;
+    --targetChildrenInput.value;
   }
-  if (e.target.parentElement.querySelector('.header__people_children input').value <= 0) {
-    e.target.parentElement.querySelector('.header__people_children input').value = 0;
+  if (targetChildrenInput.value <= 0) {
+    targetChildrenInput.value = 0;
     minusChild.style.border = '1px solid #CECECE';
     minusChild.style.color = '#CECECE';
   } else {
     minusChild.style.border = '1px solid #3077c6';
     minusChild.style.color = '#3077c6';
   }
-  if (e.target.parentElement.querySelector('.header__people_children input').value >= 10) {
-    e.target.parentElement.querySelector('.header__people_children input').value = 10;
+  if (targetChildrenInput.value >= 10) {
+    targetChildrenInput.value = 10;
     plusChild.style.border = '1px solid #CECECE';
     plusChild.style.color = '#CECECE';
   } else {
     plusChild.style.border = '1px solid #3077c6';
     plusChild.style.color = '#3077c6';
   }
-  childrenInput.setAttribute('placeholder', `${e.target.parentElement.querySelector('.header__people_children input').value}` + ' Children   — ');
+  childrenInput.setAttribute('placeholder', `${targetChildrenInput.value}` + ' Children   — ');
 };
 
 minusChild.addEventListener('click', clickChildBtn);
 plusChild.addEventListener('click', clickChildBtn);
 
 const clickRoom = (e) => {
+  const targetRoomsInput = e.target.parentElement.querySelector('.header__people_rooms input');
   if (e.target.classList.contains('plus')) {
-    ++e.target.parentElement.querySelector('.header__people_rooms input').value;
+    ++targetRoomsInput.value;
   } else if (e.target.classList.contains('minus')) {
-    --e.target.parentElement.querySelector('.header__people_rooms input').value;
+    --targetRoomsInput.value;
   }
-  if (e.target.parentElement.querySelector('.header__people_rooms input').value <= 1) {
-    e.target.parentElement.querySelector('.header__people_rooms input').value = 1;
+  if (targetRoomsInput.value <= 1) {
+    targetRoomsInput.value = 1;
     minusRoom.style.border = '1px solid #CECECE';
     minusRoom.style.color = '#CECECE';
   } else {
     minusRoom.style.border = '1px solid #3077c6';
     minusRoom.style.color = '#3077c6';
   }
-  if (e.target.parentElement.querySelector('.header__people_rooms input').value >= 30) {
-    e.target.parentElement.querySelector('.header__people_rooms input').value = 30;
+  if (targetRoomsInput.value >= 30) {
+    targetRoomsInput.value = 30;
     plusRoom.style.border = '1px solid #CECECE';
     plusRoom.style.color = '#CECECE';
   } else {
     plusRoom.style.border = '1px solid #3077c6';
     plusRoom.style.color = '#3077c6';
   }
-  roomsInput.setAttribute('placeholder', `${e.target.parentElement.querySelector('.header__people_rooms input').value}` + ' Room');
+  roomsInput.setAttribute('placeholder', `${targetRoomsInput.value}` + ' Room');
 };
 
 plusRoom.addEventListener('click', clickRoom);
